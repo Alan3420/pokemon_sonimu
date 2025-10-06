fdf7f2e5c92699fd031767e8130c497147422851
Alan3420 <alan.n.m143@gmail.com>
2025-10-06 18:07:28 +0200

    act pokemons.html

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ from pathlib import Path
 app = Flask(__name__, template_folder='templates')
 
 with open("data/pokemon.json", encoding="utf-8") as f:
-    app.config["DATA"] = json.load(f)
+    app.config["data"] = json.load(f)
     
 @app.route('/')
 def Bienvenido():
@@ -13,7 +13,7 @@ def Bienvenido():
 
 @app.route('/pokedex/')
 def Pokedex():
-    return render_template('pokemons.html')
+    return render_template('pokemons.html', pokemons = current_app.config["data"])
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=8080)
