ca65eb0cc56411733dd47a3772eb6d6925db1ed4
Alan3420 <alan.n.m143@gmail.com>
2025-10-07 16:22:58 +0200

    add pokemonID

--- app/main.py
+++ app/main.py
@@ -15,5 +15,9 @@ def Bienvenido():
 def Pokedex():
     return render_template('pokemons.html', pokemons = current_app.config["data"])
 
+@app.route('/pokedex/details')
+def PokedexDetails():
+    return render_template('pokemonsID.html', pokemons = current_app.config["data"])
+
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=8080)
