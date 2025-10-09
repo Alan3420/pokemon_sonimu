8aa97024ab6e4626d6571ddbda85c1bb60dd27d7
Alan3420 <alan.n.m143@gmail.com>
2025-10-09 18:04:35 +0200

    Actualizacion de pokemonID

--- app/main.py
+++ app/main.py
@@ -23,9 +23,26 @@ def PokedexDetails(id):
         if i['id'] == id:
             idPokemon = i
             break
+    
+    colorM = {
+        "fire": "red",
+        "dragon": "purple",
+        "grass": "green",
+        "normal": "gray",
+        "fighting": "brown",
+        "ground": "brown",
+        "water": "blue",
+        "dark":"pink",
+        "flying":"aqua",
+        "fairy": "salmon",
+        "electric": "yellow",
+        "steel": "gray",
+        "poison": "purple",
+        "ice": "white"
+    }
 
 
-    return render_template('pokemonsID.html', pokemon = idPokemon)
+    return render_template('pokemonsID.html', pokemon = idPokemon, colorM = colorM)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=8080)
