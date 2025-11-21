53b13edd262a05b38974ccdd2bd8b1a6ffe042e3
marcosfergar <marcos.fergar@educa.jcyl.es>
2025-11-21 16:40:01 +0100

    act_alchemyst

--- app/main.py
+++ app/main.py
@@ -2,19 +2,26 @@ from flask import Flask
 from app.routes.batalla_routes import batalla_pb
 from app.routes.home_routes import home_pb
 from app.routes.pokedex_route import pokedex_pb
-from flask import Flask, session
 from flask_session import Session
+# from app.database import db
 
 app = Flask(__name__, template_folder='templates')
 app.secret_key = "pokemonSonimu"
-
+# Configuracion session
 app.config["SESSION_TYPE"] = "filesystem"   # Guardar en ficheros
 app.config["SESSION_PERMANENT"] = True     # Sesiones temporales
 app.config["SESSION_FILE_DIR"] = "./.flask_session"  # Carpeta donde se guardan
+
+# Configuracion alchemy
+# app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:////data/pokemons.db.sqlite"
+# app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+#Clave session
 app.secret_key = "clave_super_secreta"
 
 # Inicializar la extensión
 Session(app)
+# db.init_app(app)
 
 app.register_blueprint(batalla_pb, url_prefix='/')
 app.register_blueprint(home_pb, url_prefix='/')
