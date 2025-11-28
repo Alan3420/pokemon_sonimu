cc89239d63874933c0c7512cdcdee51b355dc659
marcosfergar <marcos.fergar@educa.jcyl.es>
2025-11-28 16:48:30 +0100

    mas excepciones

--- app/routes/pokedex_route.py
+++ app/routes/pokedex_route.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, current_app, render_template
 import app.colors as color
 from app.services import pokemon_services
+import app.models.exceptions as Exception
 
 pokedex_pb = Blueprint('pokedex_route',__name__,template_folder='templates')
 
@@ -12,7 +13,13 @@ def Pokedex():
 @pokedex_pb.route('/<int:id>/')
 def PokedexDetails(id):
 
-    idPokemon = pokemon_services.obtener_pokemon_por_id(id)
-
+    try:
+        idPokemon = pokemon_services.obtener_pokemon_por_id(id)
+        if idPokemon is None:
+            raise Exception.PokemonNoEncontrado(
+                f"El Pokémon no se encuentra en la Pokedex."
+            )
+    except Exception.PokemonNoEncontrado as e:
+        return render_template("error404.html", mensaje=str(e)), 404
 
     return render_template('pokemonsID.html', pokemon = idPokemon, colorM = color.colorM)
