f0a1b189b41876f5fbdb050917c122c649c7f262
marcosfergar <marcos.fergar@educa.jcyl.es>
2025-12-02 17:03:26 +0100

    act script

--- setup.ps1
+++ setup.ps1
@@ -1,22 +1,23 @@
 Write-Host "============================="
-Write-Host " Inicializando entorno Python"
+Write-Host " Inicializando Instalación"
 Write-Host "============================="
 
 # 1. Crear entorno virtual
-Write-Host "Creando entorno virtual .venv..."
+Write-Host "Creando entorno virtual .venv"
 python -m venv .venv
 
 # 2. Activar el entorno virtual
-Write-Host "Activando entorno virtual..."
+Write-Host "Activando entorno virtual"
 . .\.venv\Scripts\Activate.ps1
 
 # 3. Instalar dependencias
+# Test-Path comprueba si existe
 if (Test-Path "requirements.txt") {
     Write-Host "Instalando dependencias desde requirements.txt..."
     pip install -r requirements.txt
 } else {
-    Write-Host "No se encontro requirements.txt. Instalando Flask..."
-    pip install flask
+    Write-Host "No se encontro requirements.txt."
+    return
 }
 
 # 4. Ver versiones instaladas
@@ -31,5 +32,5 @@ Write-Host "Ejecutando la aplicación..."
 python -m app.main
 
 Write-Host "============================="
-Write-Host "      Proceso completado"
+Write-Host "      Instalación completa"
 Write-Host "============================="
