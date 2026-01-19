7260624ea10550b8d34ac7b3872d76ac3bcb7b11
marcosfergar <marcos.fergar@educa.jcyl.es>
2026-01-19 17:21:21 +0100

    act_all

--- app/clients/pokemon_clients.py
+++ app/clients/pokemon_clients.py
@@ -1,4 +1,12 @@
 import requests
 
 url = "https://pokeapi.co/api/v2/pokemon?limit=20"
+
+# Ejemplo parametros
+# params = {"q": "laptop"}
+# response = requests.get(url, params=params)
+
 response = requests.get(url)
+
+if response.status_code == 200:
+    data = response.json()
